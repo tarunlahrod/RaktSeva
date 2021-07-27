bd63c9cdaf72d091b43b815be069bc2af4c6577e
Tarun <lahrod.tarun46@gmail.com>
2021-07-27 19:32:39 +0530

    Added App Logo in png and gif formats. - All the app logo stuff is in app logo directory (all designed by myself). - Added Splash Screen. - Removed User/Admin buttons from MainActivity. - App is 90% complete. *happy noises*
    Next to do:-
    1. On deleting profile, the app crashes.
    2. Whenever any change is made to the usersList (like, a new user added or removed) the updated list gets "Appended" to the existing usersList in the RecyclerView until the activity is reopened.

--- app/src/main/java/com/example/raktseva/MainActivity.java
+++ app/src/main/java/com/example/raktseva/MainActivity.java
@@ -1,57 +1,63 @@
 package com.example.raktseva;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button bt_admin_login, bt_user_login;
     FirebaseAuth mAuth;
+    Animation topAnim, bottomAnim;
+    ImageView iv_logo;
+    TextView tv_appName;
+    private static int SPLASH_SCREEN = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("User Type");
+        getSupportActionBar().hide();
 
         mAuth = FirebaseAuth.getInstance();
 
-        bt_admin_login = findViewById(R.id.bt_admin_login);
-        bt_user_login = findViewById(R.id.bt_user_login);
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        iv_logo = findViewById(R.id.splash_screen_gif);
+        tv_appName = findViewById(R.id.tv_appName);
 
-        bt_admin_login.setOnClickListener(new View.OnClickListener() {
+        iv_logo.setAnimation(topAnim);
+        tv_appName.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AdminLoginActivity.class);
+            public void run() {
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                Intent intent;
+                if (currentUser != null) {
+                    // if user already logged in, move to MainMenuActivity
+                    intent = new Intent(MainActivity.this, MainMenuActivity.class);
+                }
+                // else move to login page
+                else {
+                    intent = new Intent(MainActivity.this, LoginActivity.class);
+                }
                 startActivity(intent);
+                finish();
             }
-        });
-
-        bt_user_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser != null) {
-            Intent intent = new Intent(this, MainMenuActivity.class);
-            startActivity(intent);
-            finish();
-        }
+        }, SPLASH_SCREEN);
     }
 }
